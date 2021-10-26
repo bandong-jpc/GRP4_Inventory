06460c2108e79cf384136ce60cd14320ae1e9345
bandong-jpc <bandong.jpc@gmail.com>
2021-10-26 17:17:09 +0800

    Add new header file containing item struct definition, working on update function

---
+++ header.h
@@ -0,0 +1,7 @@
+typedef struct {
+    int id;                 //Item ID
+    char description[50];   //Item Description
+    int qty;                //Item Quantity
+    char exp[10];           //Item Expiry Date
+    double price;           //Item price
+  } item;
