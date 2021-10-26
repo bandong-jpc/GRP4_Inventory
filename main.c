88779fc18f426e4adf97fc3ef133dd57985cfbdd
bandong-jpc <bandong.jpc@gmail.com>
2021-10-26 14:14:24 +0800

    Add main menu, switch case for function calls

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  
+  char option = 'x';
+
+  do
+  {
+    printf("\n\nMAIN MENU");
+    printf("\n[A] ADD INVENTORY ITEM");
+    printf("\n[B] UPDATE INVENTORY ITEM");
+    printf("\n[C] VIEW INVENTORY LIST");
+    printf("\n[D] SEARCH INVENTORY LIST");
+    printf("\n[E] DELETE INVENTORY ITEM");
+    printf("\n[X] EXIT PROGRAM");
+
+    printf("\n\nPLEASE INPUT CHOICE: ");
+
+    scanf("%c", &option);
+
+    switch (option)
+    {
+    case 'a':
+      /* code */
+      break;
+    case 'b':
+      /* code */
+      break;
+    case 'c':
+      /* code */
+      break;
+    case 'd':
+      /* code */
+      break;
+    case 'e':
+      /* code */
+      break;
+    case 'x':
+      break;
+
+    default:
+      printf("\n\nINVALID OPTION\n\n");
+    }
+
+  } while (
+    option!='x'
+  );
+  
+
+
+  return 0;
+}
