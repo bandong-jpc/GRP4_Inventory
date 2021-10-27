adb49b4d4a99faed58ef665b8d329fc1d3e3fd8b
bandong-jpc <bandong.jpc@gmail.com>
2021-10-27 10:54:26 +0800

    Change price data type to float in struct def

--- header.h
+++ header.h
@@ -1,8 +1,7 @@
 typedef struct {
-
     int id;                 //Item ID
     char description[50];   //Item Description
     int qty;                //Item Quantity
     char exp[10];           //Item Expiry Date
-    double price;           //Item price
-} item;
+    float price;           //Item price
+  } item;
