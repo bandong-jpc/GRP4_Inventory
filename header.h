aef669c792cf8c411a5951c15e78aaa71c255608
bandong-jpc <bandong.jpc@gmail.com>
2021-10-27 15:42:01 +0800

    Add code to update function to read the file line by line

--- header.h
+++ header.h
@@ -5,3 +5,31 @@ typedef struct {
     char exp[10];           //Item Expiry Date
     float price;           //Item price
   } item;
+
+typedef struct{
+  item *array;
+  size_t used;
+  size_t size;
+} DynamicArray;
+
+void initArray(DynamicArray *a, size_t initialSize) {
+  a->array = malloc(initialSize * sizeof(item));
+  a->used = 0;
+  a->size = initialSize;
+}
+
+void insertArray(DynamicArray *a, item element) {
+  // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
+  // Therefore a->used can go up to a->size 
+  if (a->used == a->size) {
+    a->size *= 2;
+    a->array = realloc(a->array, a->size * sizeof(item));
+  }
+  a->array[a->used++] = element;
+}
+
+void freeArray(DynamicArray *a) {
+  free(a->array);
+  a->array = NULL;
+  a->used = a->size = 0;
+}
--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include "header.h"
 
 void update();
+void filecheck();
 
 int main()
 {
@@ -56,11 +57,15 @@ int main()
 }
 
 void update(){
-
+  //item variable to store read data
+  item temp;
   //check if file exists
   filecheck();
-
-  item x;
+  //dynamic array to store all of the file contents
+  DynamicArray list;
+  //initialize the array
+  initArray(&list, 1);
+  
   //File pointer
   FILE *fpointer = fopen("Inventory.csv", "r+");
   //line variable for reading line
@@ -68,7 +73,7 @@ void update(){
   //entries variable for storing number of entries
   int entries = 0;
   //variable for storing index of item to be updated
-  int pos = -1
+  int pos = -1;
 
   //loop through file reading every line
   while(!feof(fpointer)){
@@ -87,24 +92,24 @@ void update(){
       switch (i)
       {
       case 0:
-        strcpy(x.id, detail);
+        strcpy(temp.id, detail);
 
         break;
       
       case 1:
-        strcpy(x.description, detail);
+        strcpy(temp.description, detail);
         break;
       
       case 2:
-        x.qty = atoi(detail);
+        temp.qty = atoi(detail);
         break;
       
       case 3:
-        strcpy(x.exp, detail);
+        strcpy(temp.exp, detail);
         break;
       
       case 4:
-        x.price = atof(detail);
+        temp.price = atof(detail);
         break;
     
       default:
@@ -112,9 +117,21 @@ void update(){
       }
       i++;
    }
+
+    insertArray(&list, temp);
+
     entries++;
   }
   fclose(fpointer);
+
+  for (int j = 0; j < entries; j++)
+  {
+    printf("\n%d\t%s", j, list.array[j].id);
+  }
+  
+
+  freeArray(&list);
+
 }
 
 
