17b077c973e832f3c778b779b910b6f9c276e5fc
Romzk <92768906+menl3nget@users.noreply.github.com>
2021-10-28 11:45:41 +0800

    Add files via upload

--- header.h
+++ header.h
@@ -1,7 +1,7 @@
-typedef struct {
+typedef struct inventoryText{
     char id[6];             //Item ID
     char description[41];   //Item Description
     unsigned int qty;       //Item Quantity
     char exp[10];           //Item Expiry Date
     float price;           //Item price
-  } item;
+  } inventoryText;
