4b22dc1dcc2c8cfe83b86b9160933f70be3dd51b
Maynard-jmm <92769068+Maynard-jmm@users.noreply.github.com>
2021-10-28 15:20:33 +0800

    Update main.c
    A search function was added to main.c .

--- main.c
+++ main.c
@@ -73,3 +73,68 @@ void filecheck(){
   //close file to read
   fclose(fpointer);
 }
+
+
+
+
+int readFile() // read file function
+{
+    int n = 0;
+    int i;
+    FILE *f;
+    
+    f = fopen("inventory.csv", "+r");
+    if (f == NULL)
+        return -1;
+    fscanf(f, "%d\n", &n);
+    for (i = 0; i < n; ++i)
+    {
+        fgets(x[i].id, 10, f);
+        x[i].id[strlen(x[i].id) - 1] = 0; 
+        fgets(x[i].description, 20, f);
+        x[i].description[strlen(prod[i].description)-1] = 0; 
+        fscanf(f, "%d", &x[i].qty);
+        fscanf(f, "%d", &x[i].exp);
+        fscanf(f, "%f", &x[i].price);
+    }
+    fclose(f);
+    return n;
+}
+
+
+void searchInvItem(){
+	
+
+	int i;
+    char id[10];
+    int z = false;
+    item x;
+    count = readFile();
+    printf("\nItem ID: ");
+    
+	fflush(stdin);
+	scanf("%s", &id);
+	//gets(id);
+    for (i=0; i<count; i++){
+        if (strcmp(id,x[i].id)==0) 	// compare Item ID to list.
+        {
+        	z=true;
+        printf("\nItem found! Containing: \n");//...then display the match
+        printf("\nProduct name: %s",x[i].id);
+	   	printf("\nProduct name: %s",x[i].description);
+		printf("\nPrice: %d",x[i].qty);
+		printf("\nquantity: %d",x[i].exp);
+		printf("\ndiscount: %f\n\n",x[i].price);
+	
+			}
+	
+	}
+ if(z==false){	//if the Item ID doesn't exist.
+ 
+	printf("Cant find the product id: %s.",id);
+}
+	
+
+}
+
+
