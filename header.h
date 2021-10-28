9792352da3a0ae2b33c9357461d581efeafd87bf
bandong-jpc <bandong.jpc@gmail.com>
2021-10-28 08:26:16 +0800

    Add reading file in update function

--- header.h
+++ header.h
@@ -6,30 +6,82 @@ typedef struct {
     float price;           //Item price
   } item;
 
-typedef struct{
-  item *array;
-  size_t used;
-  size_t size;
-} DynamicArray;
+struct Node{
+  item data;
+  struct Node *next;
+};
 
-void initArray(DynamicArray *a, size_t initialSize) {
-  a->array = malloc(initialSize * sizeof(item));
-  a->used = 0;
-  a->size = initialSize;
+void append(struct Node** head_ref, char* id, char* desc, unsigned int qty, char* exp, float price )
+{
+    /* 1. allocate node */
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+ 
+    struct Node *last = *head_ref;  /* used in step 5*/
+  
+    /* 2. put in the data  */
+    strcpy(new_node->data.id, id);
+    strcpy(new_node->data.description, desc);
+    new_node->data.qty = qty;
+    strcpy(new_node->data.exp, exp);
+    new_node->data.price = price;
+ 
+    /* 3. This new node is going to be the last node, so make next
+          of it as NULL*/
+    new_node->next = NULL;
+ 
+    /* 4. If the Linked List is empty, then make the new node as head */
+    if (*head_ref == NULL)
+    {
+       *head_ref = new_node;
+       return;
+    } 
+      
+    /* 5. Else traverse till the last node */
+    while (last->next != NULL)
+        last = last->next;
+  
+    /* 6. Change the next of last node */
+    last->next = new_node;
+    return;   
 }
 
-void insertArray(DynamicArray *a, item element) {
-  // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
-  // Therefore a->used can go up to a->size 
-  if (a->used == a->size) {
-    a->size *= 2;
-    a->array = realloc(a->array, a->size * sizeof(item));
-  }
-  a->array[a->used++] = element;
+/* Function to delete the entire linked list */
+void deleteList(struct Node** head_ref)
+{
+   /* deref head_ref to get the real head */
+   struct Node* current = *head_ref;
+   struct Node* next;
+ 
+   while (current != NULL)
+   {
+       next = current->next;
+       free(current);
+       current = next;
+   }
+   
+   /* deref head_ref to affect the real head back
+      in the caller. */
+   *head_ref = NULL;
+
+   return;
 }
 
-void freeArray(DynamicArray *a) {
-  free(a->array);
-  a->array = NULL;
-  a->used = a->size = 0;
+void showList(struct Node** head_ref){
+ 
+    struct Node *last = *head_ref;  /* used in step 5*/
+ 
+    
+    if (*head_ref == NULL)
+    {
+      printf("\nEMPTY LIST");
+       return;
+    } 
+      
+    /* 5. Else traverse till the last node */
+    while (last->next != NULL){
+      printf("\n%s", last->data.id);
+      last = last->next;
+    }
+        
+    return;  
 }
--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include "header.h"
 
 void update();
-void filecheck();
+int entry_counter();
 
 int main()
 {
@@ -57,99 +57,139 @@ int main()
 }
 
 void update(){
-  //item variable to store read data
-  item temp;
-  //check if file exists
-  filecheck();
-  //dynamic array to store all of the file contents
-  DynamicArray list;
-  //initialize the array
-  initArray(&list, 1);
   
+  item x;
+
+  int ent = entry_counter();
+
+ /*  char id[255];
+  //File pointer
+  FILE *fpointer = fopen("Inventory.csv", "r+");
+
+  fgets(id, 255, fpointer);
+
+  fclose(fpointer);
+
+    char detail[50];
+    int i = 0;
+   // Extract the first token
+   char * token = strtok(id, ",\"");
+   // loop through the string to extract all other tokens
+   while( token != NULL ) {
+      
+      strcpy(detail, token);
+     
+      token = strtok(NULL, ",\"");
+
+
+      switch (i)
+      {
+      case 0:
+        strcpy(x.id, detail);
+        break;
+      
+      case 1:
+        strcpy(x.description, detail);
+        break;
+      
+      case 2:
+        x.qty = atoi(detail);
+        break;
+      
+      case 3:
+        strcpy(x.exp, detail);
+        break;
+      
+      case 4:
+        x.price = atof(detail);
+        break;
+      
+
+      
+      default:
+        break;
+      }
+
+       
+      i++;
+   }
+
+  printf( "\n%s", x.id ); //printing each token
+  printf( "\n%s", x.description ); //printing each token
+  printf( "\n%d", x.qty ); //printing each token
+  printf( "\n%s", x.exp ); //printing each token
+  printf( "\n%.2f", x.price ); //printing each token */
+  
+}
+
+//function for counting the number of entries in the file
+int entry_counter(){
+  struct Node* head = NULL;
+  item x;
   //File pointer
   FILE *fpointer = fopen("Inventory.csv", "r+");
   //line variable for reading line
   char line[255];
   //entries variable for storing number of entries
   int entries = 0;
-  //variable for storing index of item to be updated
-  int pos = -1;
-
-  //loop through file reading every line
+char detail[50];
+    int i = 0;
+  //check for how many entries in the file
   while(!feof(fpointer)){
     fgets(line, 255, fpointer); 
 
-  char detail[50];
-  int i = 0;
+    strcpy(detail, line);
+    i=0;
    // Extract the first token
    char * token = strtok(detail, ",\"");
    // loop through the string to extract all other tokens
    while( token != NULL ) {
+      
       strcpy(detail, token);
      
       token = strtok(NULL, ",\"");
 
+
       switch (i)
       {
       case 0:
-        strcpy(temp.id, detail);
-
+        strcpy(x.id, detail);
         break;
       
       case 1:
-        strcpy(temp.description, detail);
+        strcpy(x.description, detail);
         break;
       
       case 2:
-        temp.qty = atoi(detail);
+        x.qty = atoi(detail);
         break;
       
       case 3:
-        strcpy(temp.exp, detail);
+        strcpy(x.exp, detail);
         break;
       
       case 4:
-        temp.price = atof(detail);
+        x.price = atof(detail);
         break;
-    
+      
+
+      
       default:
         break;
       }
+
+       
       i++;
    }
 
-    insertArray(&list, temp);
+    append(&head, x.id, x.description, x.qty, x.exp, x.price);
 
     entries++;
   }
+
   fclose(fpointer);
 
-  for (int j = 0; j < entries; j++)
-  {
-    printf("\n%d\t%s", j, list.array[j].id);
-  }
-  
+  showList(&head);
 
-  freeArray(&list);
-
-}
-
-
-
-void filecheck(){
-  //open file to read
-  FILE *fpointer = fopen("Inventory.csv", "r+");
-  
-
-  //check if file exists. if not, create a new file with no content
-  if(!fpointer){
-    fclose(fpointer); //close current pointer to file
-    fpointer = fopen("Inventory.csv", "w"); //open new file for writing
-    printf("\nInventory.csv DOES NOT EXISTS. CREATING NEW FILE. \n");
-    fprintf(fpointer, "", "");
-  }
-  
-
-  //close file to read
-  fclose(fpointer);
+  return entries;
 }
