d5a9b98482469dcea9e649cb6e71b6107272a66d
bandong-jpc <77478744+bandong-jpc@users.noreply.github.com>
2021-11-01 10:56:24 +0800

    update header.h

--- header.h
+++ header.h
@@ -2,7 +2,7 @@ typedef struct {
     char id[6];             //Item ID
     char description[41];   //Item Description
     unsigned int qty;       //Item Quantity
-    char exp[10];           //Item Expiry Date
+    char exp[11];           //Item Expiry Date
     float price;           //Item price
   } item;
 
